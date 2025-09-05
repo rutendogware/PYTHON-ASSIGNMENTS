dd5377047cddfbca1ed2a31e80c76612a376fec7
rutendogware <rutendogware768@gmail.com>
2025-09-05 16:14:08 +0200

    Add files via upload

---
+++ Assinment 1.py
@@ -0,0 +1,12 @@
+
+#Ass# Constant (by convention, constants are written in uppercase)
+PI = 3.14159
+
+# Variables
+radius = 5
+area = PI * radius ** 2
+
+# Print statements
+print("Radius of the circle:", radius)
+print("Area of the circle:", area)
+inment 1
